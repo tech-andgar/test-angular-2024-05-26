1f5d3d71e5bcf45508f34c2b9bf9e2bb680d310b
Andrés García <dev@tech-andgar.me>
2024-05-24 17:29:51 -0500

    feat: updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent, FooterComponent, MainComponent } from '@components//components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  // template: ``,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
