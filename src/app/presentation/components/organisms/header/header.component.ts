81736bac8a87e44c9e00ef12b0f9c9efb2746c16
Andrés García <dev@tech-andgar.me>
2024-05-25 17:15:30 -0500

    feat add header

--- src/app/presentation/components/organisms/header/header.component.ts
+++ src/app/presentation/components/organisms/header/header.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  @HostListener('window:scroll', []) onWindowScroll() {
+    if (window.scrollY > this.sticky!) {
+      this.header?.classList.add('sticky');
+    } else {
+      this.header?.classList.remove('sticky');
+    }
+  }
 
+  header = document.getElementById('myHeader');
+  sticky = this.header?.offsetTop;
 }
